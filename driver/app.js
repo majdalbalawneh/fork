8fc8c00610bbc1a62daf162698b36dd66a59805f
majdalbalawneh <majd3180157@gmail.com>
2022-04-06 12:03:42 +0300

    fork

---
+++ driver/app.js
@@ -0,0 +1,17 @@
+`use strict`
+// 1. Write a JavaScript function that return reverse a array. 
+// Example x = 834848;
+// Expected Output : 848438
+
+console.log("task1.driver");
+var driver =[8,3,4,8,4,8];
+ driver.shift(0);
+ driver.shift(2);
+ driver.shift(1);
+ driver.shift(2);
+ driver.shift(0);
+console.log(driver);
+
+
+
+
